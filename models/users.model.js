a094aac30815ef17d72db7d9c26b789dcf73e701
DulanjayaSandaruwan <dulanjayasandaruwan1998@gmail.com>
2022-10-01 14:54:20 +0530

    complete login

--- models/users.model.js
+++ models/users.model.js
@@ -21,38 +21,4 @@ const usersSchema = new Schema({
   },
 });
 
-usersSchema.pre('save', function(next) {
-	const users = this;
-	if (!users.isModified('password')) {
-		return next();
-	}
-	bcrypt.genSalt(10, (err, salt) => {
-		if (err) {
-			return next(err);
-		}
-		bcrypt.hash(users.password, salt, (err, hash) => {
-			if (err) {
-				return next(err);
-			}
-			users.password = hash;
-			next();
-		});
-	});
-});
-
-usersSchema.methods.comparePassword = function(candidatePassword) {
-	const users = this;
-	return new Promise((resolver, reject) => {
-		bcrypt.compare(candidatePassword, users.password, (err, isMatch) => {
-			if (err) {
-				return reject(err);
-			}
-			if (!isMatch) {
-				return reject(err);
-			}
-			resolver(true);
-		});
-	});
-};
-
 module.exports = mongoose.model("Users", usersSchema);
--- routes/Users.js
+++ routes/Users.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const Users = require("../models/users.model");
 const router = express.Router();
-const jwt = require("jsonwebtoken");
-const jwtKey = "jnsdnfdju38h";
-
 const app = express();
 
 app.use(express.json());
@@ -16,32 +13,53 @@ router.post("/register", async (req, res) => {
       contact: req.body.contact,
       password: req.body.password,
     });
-    await users.save().then((data) => {
-      console.log(data);
-      const token = jwt.sign({ userId: users._id }, jwtKey);
-      res.send({ token });
-    });
+    const response = await users.save();
+    res.json(response);
+    console.log(req.body);
   } catch (error) {
     res.status(422).send(error.message);
+    console.log(req.body);
   }
 });
 
-router.post("/sign-in", async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(422).send({ error: "must provide email or password" });
-  }
-  const users = await Users.findOne({ email });
-  if (!users) {
-    return res.status(422).send({ error: "must provide email or password" });
-  }
+router.get("/", async (req, res) => {
   try {
-    await users.comparePassword(password);
-    const token = jwt.sign({ userId: users._id }, jwtKey);
-    res.send({ token });
+    const users = await Users.find();
+    res.json(users);
   } catch (err) {
-    return res.status(422).send({ error: "must provide email or password" });
+    res.send("Err:" + err);
   }
 });
+
+router.get("/loginCheck", async (req, resp) => {
+  try {
+    let res = await Users.find();
+    let response = undefined;
+    res.forEach(async (e) => {
+      if ((e.email === req.query.email) & (e.password === req.query.password)) {
+        response = true;
+      }
+    });
+    resp.json(response);
+  } catch (err) {
+    resp.json({ message: err });
+  }
+});
+
+// router.post("/sign-in", async (req, res) => {
+//   const { email, password } = req.body;
+//   if (!email || !password) {
+//     return res.status(422).send({ error: "must provide email or password" });
+//   }
+//   const users = await Users.findOne({ email });
+//   if (!users) {
+//     return res.status(422).send({ error: "must provide email or password" });
+//   }
+//   try {
+//     await users.comparePassword(password);
+//   } catch (err) {
+//     return res.status(422).send({ error: "must provide email or password" });
+//   }
+// });
 
 module.exports = router;
